d50137915beefd89d33e48ec5a373ba073df7146
Vladislav <mvd-96@mail.ru>
2023-01-28 17:36:28 +0300

    Сохранение задач

---
+++ Задача_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число А");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b * b == a)
+{
+    Console.WriteLine($"Является число {b} квадратом числа {a}");
+}
+else
+{
+    Console.WriteLine($"Не является число {b} квадратом числа {a}");
+}
