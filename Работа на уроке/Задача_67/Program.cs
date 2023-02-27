3bbd3146dca3e18ba59a43707d922e38d8b87970
Vladislav <mvd-96@mail.ru>
2023-02-27 18:38:51 +0300

    Задачи 64, 66, 68

---
+++ Работа на уроке/Задача_67/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая будет принимать
+на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9*/
+Console.Clear();
+int Recursiv(int N)
+{
+    if (N == 0) return 0;
+    else
+    {
+        return N % 10 + Recursiv(N / 10);
+    }
+}
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Recursiv(N));
---
+++ Работа на уроке/Задача_69/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая на вход принимает два числа A и B,
+и возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8*/
+Console.Clear();
+int Recursiv(int A, int B)
+{
+    if (B == 0) return 1;
+    else
+    {
+        return A*Recursiv(A,B-1);
+    }
+}
+Console.WriteLine("Введите число A");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите степень B");
+int B = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Recursiv(A,B));
